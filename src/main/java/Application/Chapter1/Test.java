bb3f287259c6d72d9be0d72ebb9bceff111a9aad
krisna kanth <kkanth2904@menard-inc.com>
2023-04-20 17:36:49 -0400

    11

---
+++ src/main/java/Application/Chapter1/Test.java
@@ -0,0 +1,22 @@
+package Application.Chapter1;
+
+public class Test {
+
+        int test_a, test_b;
+        Test(int a, int b)
+        {
+            test_a = a;
+            test_b = b;
+        }
+
+    public Test() {
+        .
+    }
+
+
+    public static void main (String args[])
+        {
+            Test test = new Test();
+            System.out.println(test.test_a+" "+test.test_b);
+        }
+    }
